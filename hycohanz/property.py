0004eecf385637a4204ca48eca8ee7efb9e20037
husky <shuaike945@gmail.com>
2016-09-09 22:03:01 +0800

    replace tabs in property.py to spaces

--- hycohanz/property.py
+++ hycohanz/property.py
@@ -61,10 +61,10 @@ def set_variable(oProject, name, value):
     
     """
     if '$' in name: 
-		oProject.SetVariableValue(name,Expression(value).expr)
+        oProject.SetVariableValue(name,Expression(value).expr)
     else:
-		oDesign = oProject.GetActiveDesign()
-		oDesign.SetVariableValue(name,Expression(value).expr)
+        oDesign = oProject.GetActiveDesign()
+        oDesign.SetVariableValue(name,Expression(value).expr)
 
 def get_variables(oProject,oDesign=''):
     """
